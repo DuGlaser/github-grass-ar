48701b88e56926751b791a7d30c869c0b445fb62
DuGlaser <duglaser0618@gmail.com>
2020-07-30 21:32:18 +0900

    :sparkles: Add the Weeks model

---
+++ server/model/github_model.go
@@ -0,0 +1,10 @@
+package model
+
+import "github.com/shurcooL/githubv4"
+
+type Weeks []struct {
+	ContributionDays []struct {
+		ContributionCount githubv4.Int
+		Color             githubv4.String
+	}
+}
