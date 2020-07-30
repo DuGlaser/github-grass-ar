cb579a064b465d988ddd0c89f648f5ce32b94cb0
DuGlaser <duglaser0618@gmail.com>
2020-07-30 22:29:02 +0900

    :sparkles: Add the to load env process

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -2,14 +2,22 @@ package auth
 
 import (
 	"context"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
 )
 
 func GithubAuth() *githubv4.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "xxxxxxxxxxxxxxxxxxxxxxxx"},
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
 	)
 
 	httpClient := oauth2.NewClient(context.Background(), src)
