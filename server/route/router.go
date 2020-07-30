06f41db5ce4902443e8a699cbb11cabfe368d2aa
DuGlaser <duglaser0618@gmail.com>
2020-07-30 16:51:46 +0900

    :sparkles: Add the api router

--- server/route/router.go
+++ server/route/router.go
@@ -1,6 +1,8 @@
 package route
 
 import (
+	v1 "server/api/v1"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -12,6 +14,11 @@ func Init() *echo.Echo {
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAcceptEncoding},
 	}))
+
+	apiV1 := e.Group("/api/v1")
+	{
+		apiV1.GET("/", v1.GetContributionsInfo)
+	}
 
 	return e
 }
