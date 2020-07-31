13307896ae3e714c89003a4319977f12848304a3
DuGlaser <duglaser0618@gmail.com>
2020-07-31 10:54:19 +0900

    :recycle: Fix the error response message

--- server/api/v1/handlers/github_handler.go
+++ server/api/v1/handlers/github_handler.go
@@ -3,25 +3,26 @@ package handlers
 import (
 	"net/http"
 	"server/api/v1/controllers"
+	"server/utils"
 
 	"github.com/labstack/echo"
 )
 
 type Params struct {
-	Name string `json: "name" from:"name" query:"name"`
-	From string `json: "from" from:"from" query:"from"`
-	To   string `json: "from" from:"from" query:"from"`
+	Name string `query:"name"`
+	From string `query:"from"`
+	To   string `query:"to"`
 }
 
-func GetContributionsInfo(c echo.Context) (err error) {
+func GetContributionsInfo(c echo.Context) error {
 	p := new(Params)
-	if err = c.Bind(p); err != nil {
-		return
+	if err := c.Bind(p); err != nil {
+		return c.JSON(http.StatusInternalServerError, utils.NewError(err))
 	}
 
 	data, err := controllers.ContributionsInfoQuery(p.Name, p.From, p.To)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
+		return c.JSON(http.StatusInternalServerError, utils.NewError(err))
 	}
 
 	return c.JSON(http.StatusOK, data)
