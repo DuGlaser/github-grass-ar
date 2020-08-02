80e792a4d744bc415f2332900fcbb0ee6b92b9f6
DuGlaser <duglaser0618@gmail.com>
2020-08-02 15:26:38 +0900

    :sparkles: Add the Github class to the api directory

---
+++ unity/Assets/Scripts/Api/Github.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Api
+{
+  public class Github : Web.ApiBase
+  {
+
+    [Serializable]
+    public struct Reequest
+    {
+        public string name;
+        public string from;
+        public string to;
+    }
+
+    public Reequest reequest;
+
+    [Serializable]
+    public struct Response
+    {
+      public Weeks[] weeks;
+    }
+
+    [Serializable]
+    public struct  Weeks
+    {
+      public ContributionDays[] contributionDays;
+    }
+
+    [Serializable]
+    public struct ContributionDays 
+    {
+        public int contributionCount;
+        public string color;
+    }
+
+    public Response response;
+  }
+}
