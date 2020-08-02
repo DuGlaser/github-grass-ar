719305a7d120bccbb132c6ac2e6f9b07c31fa463
DuGlaser <duglaser0618@gmail.com>
2020-08-02 17:42:20 +0900

    :sparkles: Add the sample class

---
+++ unity/Assets/Scripts/sample.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sample : MonoBehaviour
+{
+    private Api.Github githubApi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        githubApi = new Api.Github();
+
+        sendGithubApi();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void sendGithubApi()
+    {
+        githubApi.EndPoint = "/get_contributions_info";
+        githubApi.reequest.name = "DuGlaser";
+        githubApi.reequest.from = "2020-07-19";
+        githubApi.reequest.to = "2020-07-25";
+
+        githubApi.Send<Api.Github.Request>(ref githubApi.reequest, result =>
+        {
+            if (result.isSucceeded)
+            {
+                var res = githubApi.Response<Api.Github.Response>();
+                Debug.Log(res[0].ContributionDays[0].Color);
+            }
+            else
+            {
+                Debug.Log(result.error);
+            }
+        });
+    }
+}
