094239e2e10035053e8f8bfb22ad2484c13d0290
DuGlaser <duglaser0618@gmail.com>
2020-08-04 20:48:37 +0900

    :sparkles: Add the PrefabController class

---
+++ unity/Assets/Scripts/PrefabController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject GITHUB_LEVEL_1;
+
+    [SerializeField]
+    GameObject GITHUB_LEVEL_2;
+
+    [SerializeField]
+    GameObject GITHUB_LEVEL_3;
+
+    [SerializeField]
+    GameObject GITHUB_LEVEL_4;
+
+    [SerializeField]
+    GameObject GITHUB_LEVEL_5;
+
+    private Vector3 prefabTransfrom = new Vector3(-0.35f, 0, 0);
+
+    public void CreateGithubObject(string color, string date, int count)
+    {
+        prefabTransfrom.x += .1f;
+
+        GameObject instantiatePrefab;
+
+        switch (color)
+        {
+          case "#216e39":
+            instantiatePrefab = GITHUB_LEVEL_5;
+            break;
+        
+          case "#30c14e":
+            instantiatePrefab = GITHUB_LEVEL_4;
+            break;
+
+          case "#40c463":
+            instantiatePrefab = GITHUB_LEVEL_3;
+            break;
+
+          case "#9be9a8":
+            instantiatePrefab = GITHUB_LEVEL_2;
+            break;
+
+          default:
+            instantiatePrefab = GITHUB_LEVEL_1;
+              break;
+        }
+
+        Instantiate(instantiatePrefab, prefabTransfrom, Quaternion.identity);
+    }
+}
